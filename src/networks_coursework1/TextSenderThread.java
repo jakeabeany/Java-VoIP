9eb90394e0d8d6630add1f35ed545e2a6a6d8c42
jakeabeany <jakemcvey1@gmail.com>
2017-02-13 19:03:53 +0000

    added testing

--- src/networks_coursework1/TextSenderThread.java
+++ src/networks_coursework1/TextSenderThread.java
@@ -1,11 +1,19 @@
-package networks_coursework1;
-import java.net.*;
-import java.io.*;
+package networks_labs;
+/*
+ * TextSender.java
+ *
+ * Created on 15 January 2003, 15:29
+ */
 
 /**
  *
- * @author  Matthew Williams & Jake McVey
+ * @author  abj
  */
+import java.net.*;
+import java.io.*;
+import uk.ac.uea.cmp.voip.DatagramSocket2;
+import uk.ac.uea.cmp.voip.DatagramSocket3;
+
 public class TextSenderThread implements Runnable{
     
     static DatagramSocket sending_socket;
@@ -23,7 +31,7 @@ public class TextSenderThread implements Runnable{
         //IP ADDRESS to send to
         InetAddress clientIP = null;
 	try {
-		clientIP = InetAddress.getByName("xspc");  //CHANGE localhost to IP or NAME of client machine
+		clientIP = InetAddress.getByName("DESKTOP-7D13IVJ");  //CHANGE localhost to IP or NAME of client machine
 	} catch (UnknownHostException e) {
                 System.out.println("ERROR: TextSender: Could not find client IP");
 		e.printStackTrace();
@@ -38,7 +46,7 @@ public class TextSenderThread implements Runnable{
         
         //DatagramSocket sending_socket;
         try{
-		sending_socket = new DatagramSocket();
+		sending_socket = new DatagramSocket3();
 	} catch (SocketException e){
                 System.out.println("ERROR: TextSender: Could not open UDP socket to send from.");
 		e.printStackTrace();
@@ -56,25 +64,31 @@ public class TextSenderThread implements Runnable{
         //Main loop.
         
         boolean running = true;
-        
+        int i = 1;
         while (running){
             try{
                 //Read in a string from the standard input
-                String str = in.readLine();
+                //String str = in.readLine();
+                //String str = in.readLine();
                 
-                //Convert it to an array of bytes
-                byte[] buffer = str.getBytes();
-               
-                //Make a DatagramPacket from it, with client address and port number
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, clientIP, PORT);
-            
-                //Send it
-                sending_socket.send(packet);
+                String str = null;
                 
-                //The user can type EXIT to quit
-                if (str.equals("EXIT")){
-                    running=false;
+                if(i == 100)
+                    running = false;
+                
+                for(i = 0; i < 100; i++){
+                    str = "packet" + (i+1) + "\n";
+                    
+                    //Convert it to an array of bytes
+                    byte[] buffer = str.getBytes();
+
+                    //Make a DatagramPacket from it, with client address and port number
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, clientIP, PORT);
+
+                    //Send it
+                    sending_socket.send(packet);
                 }
+                
 
             } catch (IOException e){
                 System.out.println("ERROR: TextSender: Some random IO error occured!");
